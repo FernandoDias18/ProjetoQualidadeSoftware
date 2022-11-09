2dfa0bec4b1d9c47bb4981294acc366398ca7e58
André Delfino <andredelphin@outlook.com>
2022-11-09 20:49:26 -0300

    mudei o input de local, para facilitar o desenvolvimento

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
+
 import './Header.css'
+
+
+
+
 export function Header(){
+
+
+    
+
     return(
     <>
         <div className="header">
         <h2 className='titulo'> CheckList</h2>
-        <div className='inputs'>
-            <input type="text" className='itens' placeholder="Descreva o item"/>
-            <button>Adicionar</button>
-        </div>
+        
         </div>
 
             
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,16 +1,63 @@
 
-import {Item} from '../Item/Item'
+import { Question } from '../Question/Question';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
+import "./List.css"
 
 export function List(){
+
+
+
+    const [question, setQuestion] = useState('')
+    const[questions, setQuestions]=useState([])
+
+
+
+    function handleNewQuestion(event){
+        setQuestion(question => event.target.value)
+
+    }
+
+    function handleCreateNewQuestions() {
+
+
+        if (question.length === 0) {
+          return
+        }
+    
+        const newQuestion= {
+          id: uuidv4(),
+          description: question,
+          yes: false,
+          no:false,
+          notAplicable:false
+    
+        }
+    
+        setQuestions([...questions, newQuestion])
+        setQuestion('')
+    
+    
+      }
+
     return(
-        
-        <ul>
-            <Item/>
-            <Item/>
-            <Item/>
-            <Item/>
-            
-        </ul>
+        <div className='container-list'>
+            <div className='inputs'>
+                <input type="text" className='itens' placeholder="Descreva o item" onChange={handleNewQuestion} value={question}/>
+                <button onClick={handleCreateNewQuestions} >Adicionar</button>
+            </div>
+            <div className='Question-list'>
+                {
+                    questions.map((question, index)=>{
+                        return <Question
+                        key={question.id}
+                        questionTitle={question.description}
+                        index={index} />
+                    })
+                } 
+
+            </div>
+        </div>
     )
 }
