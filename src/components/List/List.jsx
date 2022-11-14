4d3daa06fd5425333a56ee39cef34d60d6764188
André Delfino <andredelphin@outlook.com>
2022-11-14 00:21:35 -0300

    Habilitando a edição das questçoes

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -11,15 +11,32 @@ export function List(){
 
     const [question, setQuestion] = useState('')
     const[questions, setQuestions]=useState([])
-
+    const [editandoQuestao, setEditandoQuestao] = useState(null)
+    const [editandoQuestaoValor, seteditandoQuestaoValor] = useState('')
 
 
     function handleNewQuestion(event){
         setQuestion(question => event.target.value)
 
     }
+    function handleNewValueQuestion(event){
+        seteditandoQuestaoValor(editandoQuestaoValor => event.target.value)
+
+    }
+    
+    function handleSaveNewQuestion (){
+        
+        
+        const questionNew = questions.find(q => q.id === editandoQuestao)
+        if (!questionNew) {
+            return
+        }
+        questionNew.description = editandoQuestaoValor
+        setEditandoQuestao(null)
+    }
 
     function handleCreateNewQuestions() {
+
 
 
         if (question.length === 0) {
@@ -41,6 +58,12 @@ export function List(){
     
       }
 
+    const handleEditingQuestion =(id)=>{
+        setEditandoQuestao(id)
+        
+        
+
+    }
     return(
         <div className='container-list'>
             <div className='inputs'>
@@ -51,14 +74,25 @@ export function List(){
             <div className='Question-list'>
                 {
                     questions.map((question, index)=>{
-                        return <Question
-                        key={question.id}
-                        questionTitle={question.description}
-                        index={index} />
+                        return (
+                           <Question
+                                key={question.id}
+                                id={question.id}
+                                questionTitle={question.description}
+                                index={index}
+                                editarQuestao={handleEditingQuestion}
+                                editandoQuestao={editandoQuestao}
+                                handleNewValueQuestion={handleNewValueQuestion}
+                                editandoQuestaoValor={editandoQuestaoValor}
+                                handleSaveNewQuestion={handleSaveNewQuestion}
+                            
+                            />
+                
+                        )
                     })
                 } 
 
-            </div>
+            </div>  
         </div>
     )
 }
--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,6 +1,25 @@
 import {useState} from 'react'
 import './Question.css'
-export function Question({questionTitle,index}){
+import { GiConfirmed } from 'react-icons/gi'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export function Question({questionTitle,index,editandoQuestao ,editarQuestao, id,handleNewValueQuestion,editandoQuestaoValor, handleSaveNewQuestion}){
 
   const [resposta, setResposta] = useState('')
 
@@ -15,13 +34,32 @@ export function Question({questionTitle,index}){
         <>
           <div className="container-question">
               <span className='id-Question'>id</span>
-              <span className='Question-title'>{questionTitle}</span>
+              { id === editandoQuestao?
+                (
+                  <div className='container-input-edition'>
+                     <input 
+                      type="text" 
+                      className='Input-edition' 
+                      placeholder={questionTitle} 
+                      onChange={handleNewValueQuestion} value={editandoQuestaoValor}
+                    />
+                    <button className='Button-confirm-edition' onClick={handleSaveNewQuestion}>
+                      <GiConfirmed size={"1.5em"}/>
+                    </button>
+                   
+                  </div>
+                ):
+                (
+
+                  <span className='Question-title'>{questionTitle}</span>
+                )
+
+              }
+              <dir>
+                <button onClick={()=>editarQuestao(id)}> Editar</button>
+              </dir>
               
             <div className="Respostas">
-
-              
-
-
 
               <select className={`Resposta${resposta}`} onChange={gaurdarRespota} value={resposta}>
                 <option className='option' value="NaoSeAplica"> Não se aplica</option>
